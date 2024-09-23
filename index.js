dda50e1fb0173d7842842616c065b86587f2b00a
Gags-1 <104347087+Gags-1@users.noreply.github.com>
2024-09-24 03:42:20 +0530

    Url from dev to prod

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
         document.querySelector("#ws-id").textContent = username;
 
         // Initialize WebSocket globally
-        ws = new WebSocket(`ws://127.0.0.1:8000/ws?token=${token}`);
+        ws = new WebSocket(`http://live-chat-app-backend-au8y.onrender.com/ws?token=${token}`);
         const statusElement = document.getElementById('status');
         const userList = document.getElementById('userList');
         let onlineUsers = {};
